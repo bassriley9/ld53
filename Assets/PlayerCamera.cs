5ed4f2d2414e17757cbf054963260b8acf69d07d
unknown <firelink770@gmail.com>
2023-04-29 13:01:06 -0500

    PlayerCamera Logic, Main Scene Organization

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -22,6 +22,9 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (followTarget == null) 
+        {
+            Debug.LogWarning("WARNING: No Follow Target Assigned.");
+        }
     }
 }
